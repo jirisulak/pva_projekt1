a4031ca1775ab4c1b7bbc488798595b8db67b5e9
jirisulak <D21175@oaopava.cz>
2023-11-14 13:57:29 +0100

    a

---
+++ loop.py
@@ -0,0 +1,15 @@
+for i in range(6): #[0, 1, 2, 3, 4, 5]
+    print(i)
+
+name = "Harry"
+
+for i in name:
+    print(i)
+
+houses = {"Harry": "Gryffindor", "Draco": "Zmijozel"}
+
+def square(x):
+    return x * x
+
+for i in range(10):
+    print(f"The square of {i} is {square(i)}")
---
+++ test.py
@@ -0,0 +1,38 @@
+print("Program pro výpočet povrchu kvádru a jeho obsahu")
+     
+#a = float(input("Zadej stranu a:"))
+#b = float(input("Zadej stranu b:"))
+#c = float(input("Zadej stranu c:"))
+
+a = 5
+b = 3
+c = 2
+
+povrch = 2 * (a*b + b*c + a*c)
+obsah = a * b * c
+
+print(f"Povrch je {povrch}, obsah je {obsah}.")
+
+#.append = přidá se do seznamu(list)
+#.sort = abecední seřezení
+
+#Create an empty set
+
+s = set()
+
+# Add elements to set
+
+s.add(1)
+s.add(2)
+s.add(3)
+
+# Remove from set
+
+s.remove(2)
+
+# How many elements are in the set
+
+print()
+
+#loops
+
